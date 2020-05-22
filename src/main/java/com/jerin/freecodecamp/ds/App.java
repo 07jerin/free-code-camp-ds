3e18330edfb0efe741bb148e32176e042981fb75
Jerin Joseph <jerin.joseph@oracle.com>
2020-05-22 08:06:51 +0530

    Initial

---
+++ src/main/java/com/jerin/freecodecamp/ds/App.java
@@ -0,0 +1,13 @@
+package com.jerin.freecodecamp.ds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
